c7f295f7b8e99baebd0f72d6333fd4da3804e987
Benjamin Bulone <benja.bulone@gmail.com>
2022-03-01 11:13:11 +0100

    changements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-var menuMobile = document.getElementById('testmobile');
-
+var menuMobile = document.getElementById("testmobile");
 
 function openNav() {
-    $('.fa-bars').addClass('display-none');
-    $('.nav2').css('display', 'flex');
-    $('.fa-times').removeClass('display-none');
-    $('.fa-times').addClass('display-croix');
+  $(".nav2").slideDown("slow", function () {
+    $(".fa-bars").addClass("display-none");
+    $(".nav2").css("display", "flex");
+    $(".fa-times").removeClass("display-none");
+    $(".fa-times").addClass("display-croix");
+  });
 }
 
 function closeNav() {
-    $('.nav2').css('display', 'none');
-    $('.fa-times').removeClass('display-croix');
-    $('.fa-times').addClass('display-none');
-    $('.fa-bars').removeClass('display-none');
-    $('.fa-bars').addClass('display-block');
-
+  $(".nav2").slideUp("slow", function () {
+    $(".nav2").css("display", "none");
+    $(".fa-times").removeClass("display-croix");
+    $(".fa-times").addClass("display-none");
+    $(".fa-bars").removeClass("display-none");
+    $(".fa-bars").addClass("display-block");
+  });
 }
+
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    navigation: true, // Show next and prev buttons
+    autoplay: true,
+    slideSpeed: 10,
+    paginationSpeed: 10,
+    loop: true,
+    autoplayTimeout: 2500,
+    items: 2,
+  });
+});
